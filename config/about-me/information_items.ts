b6f89b460fd8f7eb1e3a4f5706efdc42f5311a5c
Neo Huncho <william.guinaudie@gmail.com>
2022-11-05 15:42:38 +0100

    version 1.0.3 -minor changes

--- config/about-me/information_items.ts
+++ config/about-me/information_items.ts
@@ -1,9 +1,9 @@
 const information_items = [
   "🍅 - I'm proactive and concentrated when I work (1h Pomodoro style)",
-  '💻 - I love to code (work and side-projects) and learn new things every day',
-  '🛖 - I have great messaging/calling skills and work at my optimal from home,',
-  '🤝 - But I also love to meet new people face to face and create a bond!',
-  '🌍 - I am concerned about climate change and is a large influence in my life',
+  "💻 - I'm passionate about coding (work and side-projects) and learn new things every day",
+  '🛖 - I have great messaging/calling skills and work optimally from home,',
+  '🤝 - I love to meet new people face to face and create a bond!',
+  '🌍 - I am concerned about climate change; it is a large influence in my life',
   '⛺ - I love the outdoors and enjoy hiking, biking, and climbing (amongst others!)',
   '💞 - One of my future goals is to contribute more to open source',
 ];
